b32153a45c3a66d63a03630b7126ae68c21131b3
Azka Fauzan <azkafauzanf2003@gmail.com>
2024-01-11 05:22:39 +0700

    Changed to CSS-added version

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,52 +1,54 @@
+<?php
+
+
+include "header.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
+    <!-- <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>"> -->
 </head>
 <body>
-    <header>
-        <div class="container">
-            <h1>Welcome to Our Foodie Paradise</h1>
-            <nav>
-                <ul>
-                    <li><a href="<?= site_url('home'); ?>">Home</a></li>
-                    <li><a href="<?= site_url('menu/show_menu'); ?>">Menu</a></li>
-                    <li><a href="<?= site_url('login/logout'); ?>">Logout</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-
     <section>
-        <div class="container">
-            <h2>Featured Menus</h2>
-            <?php if (!empty($menu)): ?>
-                <ul>
-                    <?php foreach ($menu as $item): ?>
-                        <li>
-                            <img src="<?= base_url('assets/images/menus/'.$item['photo']) ?>" alt="<?= $item['name']; ?>"> <br>
-                            <?= $item['name']; ?> <br>
-                            <?= $item['description']; ?> <br>
-                            <a href="<?= 'https://'.$item['whatsapp_link'] ?>"><button>Pesan</button></a> <br>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php else: ?>
-                <p>No menu available.</p>
-            <?php endif; ?>
-        </div>
+    <div class='col-md-3 col-xs-6'>
+									<div class='product-img'>
+                                    <img src="<?= base_url('assets/images/menus/'.$item['photo']) ?>" alt="<?= $item['name']; ?>"> 
+										<div class='product-label'>
+										</div>
+									</div></a>
+									<div class='product-body'>
+										<p class='product-category'><?= $item['name']; ?></p>
+										<h4 class='product-price header-cart-item-info'><?= $item['description']; ?></h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+									
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='"<?= 'https://'.$item['whatsapp_link'] ?>"'><i class='fa fa-shopping-cart'></i> pesan </button>
+									</div>
+								</div>
+                                </div>
+							
     </section>
+    <?php
 
-    <footer>
-        <div class="container">
-            <p>&copy; 2023 Foodie Paradise. All rights reserved.</p>
-        </div>
-    </footer>
+
+include "footer.php";
+
+?>
+  
     <script src="<?= base_url('assets/script.js'); ?>"></script>
 </body>
+
 </html>
 
 
